4077c09c34101541ae8eb6bc65ccb9e5d9d309b6
menomoto <menomoto@yahoo-corp.jp>
2019-06-18 19:23:19 +0900

    ref.

--- TextRecog/TextRecog/ViewController.swift
+++ TextRecog/TextRecog/ViewController.swift
@@ -4,14 +4,10 @@ import VisionKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var cardNumberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let request = VNRecognizeTextRequest { (request, error) in
-            //
-        }
-        request.recognitionLevel = .accurate
-        
     }
     
     @IBAction func didTapScanStartButton(_ sender: Any) {
@@ -22,8 +18,36 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: VNDocumentCameraViewControllerDelegate {
-    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
-        //
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        
+        dismiss(animated: true, completion: nil)
+        
+        if scan.pageCount <= 0 { return }
+        let image = scan.imageOfPage(at: 0)
+        guard let cgImage = image.cgImage else { return }
+
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        do {
+            try requestHandler.perform([reuqest()])
+        } catch {
+            print(error)
+        }
+    }
+    
+    private func reuqest() -> VNRecognizeTextRequest {
+        let request = VNRecognizeTextRequest { (request, error) in
+            guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+            var text = ""
+            for observation in observations {
+                guard let candidate = observation.topCandidates(1).first else { continue }
+                text += candidate.string + "\n"
+            }
+            
+            self.cardNumberLabel.text = text
+        }
+        request.recognitionLevel = .accurate
+
+        return request
     }
 }
 
